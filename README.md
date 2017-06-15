6ca78081987db4b1a3eeb868956381e1fa97e5b5
Lucas Vieira <lucas.samuel2002@gmail.com>
2017-06-14 23:34:56 -0300

    Changed syntax so the user can define his own prefix.

--- README.md
+++ README.md
@@ -9,6 +9,40 @@ It implements a simple Scheme API foi doing so, and is designed to run on GNU Gu
 YASWEG will then write a static HTML file from the information provided.
 The goal is to build such simple thing, for study purposes.
 
+By using YASWEG, you can generate an HTML page like
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="bootstrap.css">
+        <link rel="stylesheet" href="main.css">
+    </head>
+
+    <body>
+        <h1>My Webpage</h1>
+        <p>This is a webpage.</p>
+        <br/>
+        <p>Cool, isn't it?</p>
+    </body>
+</html>
+```
+
+By using such simple lisp-y syntax:
+
+```scheme
+(use-modules ((yasweg generator) #:prefix yas:))
+
+(yas:on-page "test.html"
+  (yas:on-head
+    (yas:link-css "main.css"))
+  (yas:on-body
+    (yas:h 1 "My Webpage")
+    (yas:p "This is a webpage.")
+    (yas:newline)
+    (yas:p "Cool, isn't it?")))
+```
+
 Usage
 -----
 YASWEG is built atop GNU Guile, so you might as well just compile YASWEG once, then import the `(yasweg generator)` module, and you're good to go.
--- test/test.scm
+++ test/test.scm
@@ -1,61 +1,61 @@
-(use-modules ((yasweg generator)))
+(use-modules ((yasweg generator) #:prefix yas:))
 
 (define (generate-file file)
-  (yasweg-on-page
+  (yas:on-page
    file
    
-   (yasweg-on-head
-    (yasweg-set-metadata '((charset utf-8)
+   (yas:on-head
+    (yas:set-metadata '((charset utf-8)
                            (title "YASWEG Static Page")
                            (author alchemist)
                            (description "Another static page generated with YASWEG.")))
-    (yasweg-link-css "main.css"))
+    (yas:link-css "main.css"))
    
-   (yasweg-on-body
+   (yas:on-body
     ;; Navigation
-    (yasweg-on-navbar
-     (yasweg-on-container
-      (yasweg-ulist 'pull-left
+    (yas:on-navbar
+     (yas:on-container
+      (yas:ulist 'pull-left
                     '(("Back to Parent" . "#about")))
-      (yasweg-ulist 'pull-right
+      (yas:ulist 'pull-right
                     '((About . about)))))
 
     ;; Big Ribbon
-    (yasweg-on-ribbon
-     (yasweg-h 1 "My First Webpage")
-     (yasweg-p "This webpage was generated using YASWEG.")
-     (yasweg-p (yasweg-inline-link "about:blank"
+    (yas:on-ribbon
+     (yas:h 1 "My First Webpage")
+     (yas:p "This webpage was generated using YASWEG.")
+     (yas:p (yas:inline-link "about:blank"
                                    "This is a test link that directs you to nothing."
                                    'blank))
-     (yasweg-button 'outline-primary "Oh please press me"))
+     (yas:button 'outline-primary "Oh please press me"))
 
-    (yasweg-on-divclass
+    (yas:on-divclass
      "basic-info"
-     (yasweg-on-divclass
+     (yas:on-divclass
       "light-block"
-      (yasweg-on-row
-       (yasweg-on-col '((md 6))
-                      (yasweg-h 3 "Light Block.")
-                      (yasweg-p "Testing.")
-                      (yasweg-newline)
-                      (yasweg-button 'outline-primary "#" "centerobj" "Press me"))
+      (yas:on-row
+       (yas:on-col '((md 6))
+                      (yas:h 3 "Light Block.")
+                      (yas:p "Testing.")
+                      (yas:newline)
+                      (yas:button 'outline-primary "#" "centerobj" "Press me"))
        
-       (yasweg-on-col '((md 6) (xs 6))
-                      (yasweg-img 'centerobj "img/lena.jpg"))))
+       (yas:on-col '((md 6) (xs 6))
+                      (yas:img 'centerobj "img/lena.jpg"))))
      
-     (yasweg-on-divclass
+     (yas:on-divclass
       "bold-block"
-      (yasweg-on-row
-       (yasweg-on-col '((md 6))
-                      (yasweg-img 'centerobj "img/lena.jpg"))
-       (yasweg-on-col '((md 6))
-                      (yasweg-h 3 "Bold block.")
-                      (yasweg-p "Testing again.")
-                      (yasweg-button 'outline-primary "Press me")))))
+      (yas:on-row
+       (yas:on-col '((md 6))
+                      (yas:img 'centerobj "img/lena.jpg"))
+       (yas:on-col '((md 6))
+                      (yas:h 3 "Bold block.")
+                      (yas:p "Testing again.")
+                      (yas:button 'outline-primary "Press me")))))
 
     ;; Footer
-    (yasweg-on-footer
-     (yasweg-p
+    (yas:on-footer
+     (yas:p
       "text-muted"
       "Copyright blablabla"))))
 
